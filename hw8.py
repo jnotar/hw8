68106288b0efd71ef2cba3c3e8fcd2a2bf05fd59
Julia Notar <jnotar@ucla.edu>
2015-02-05 04:51:20 +0000

    Add comments to script

--- hw8.py
+++ hw8.py
@@ -2,9 +2,10 @@
 
 from __future__ import division 
 
-def get_at_content(dna, sig_figs):
-    length = len(dna)
-    a_count = dna.upper().count("A")
-    t_count = dna.upper().count("T")
-    at_content = (a_count + t_count) / length
-    return round(at_content, sig_figs)
+def get_at_content(dna, sig_figs): #define function w/ arguments for dna and significant figures
+    length = len(dna) #calculate length of dna sequence
+    a_count = dna.upper().count("A") #count A bases, even if lowercase
+    t_count = dna.upper().count("T") #count T bases, even if lowercase
+    at_content = (a_count + t_count) / length #calculate AT content
+    return round(at_content, sig_figs) #return rounded AT content, to # of sig figs
+
